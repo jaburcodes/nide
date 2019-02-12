cf0d958cfade6010289ba670595d2561ff0d6132
leonardomso <leonardomso11@gmail.com>
2019-02-12 09:50:57 -0200

    Added maps with leaflet

--- packages/web/src/screens/Home/MapHome.tsx
+++ packages/web/src/screens/Home/MapHome.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import * as L from "leaflet";
+import { Children, Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 import StyledHomeWrapper from "../../utils/components/Home/Home";
 import Intro from "../../utils/components/Home/Map/Intro/Intro";
-import Map from "../../utils/components/Home/Map/Map/Map";
+// import Map from "../../utils/components/Home/Map/Map/Map";
 
 import Title from "../../utils/styles/Title/Title";
 
@@ -16,17 +18,88 @@ const Wrapper = styled.div`
     height: 70%;
 `;
 
-const MapHome = () => (
-    <StyledHomeWrapper>
-        <Intro>
-            <Wrapper>
-                <Title color="black">Para iniciar, selecione um ponto.</Title>
-            </Wrapper>
-        </Intro>
-        <Map>
-            <Title color="black">Map</Title>
-        </Map>
-    </StyledHomeWrapper>
+// custom-component.js
+interface MyPopupMarkerProps {
+    children: Children;
+    position: L.LatLngExpression;
+}
+
+interface MyMarker extends MyPopupMarkerProps {
+    key: string;
+}
+
+const MyPopupMarker = ({ children, position }: MyPopupMarkerProps) => (
+    <Marker position={position}>
+        <Popup>
+            <span>{children}</span>
+        </Popup>
+    </Marker>
 );
+
+interface MyMarkersListProps {
+    markers: MyMarker[];
+}
+
+const MyMarkersList = ({ markers }: MyMarkersListProps) => {
+    const items = markers.map(({ key, ...props }) => (
+        <MyPopupMarker key={key} {...props} />
+    ));
+    return <div style={{ display: "none" }}>{items}</div>;
+};
+
+interface CustomComponentState {
+    lat: number;
+    lng: number;
+    zoom: number;
+}
+
+class MapHome extends React.Component<{}, CustomComponentState> {
+    state = {
+        lat: 51.505,
+        lng: -0.09,
+        zoom: 13
+    };
+
+    render() {
+        const center: L.LatLngExpression = [this.state.lat, this.state.lng];
+
+        const markers: MyMarker[] = [
+            {
+                key: "marker1",
+                position: [51.5, -0.1],
+                children: "My first popup"
+            },
+            {
+                key: "marker2",
+                position: [51.51, -0.1],
+                children: "My second popup"
+            },
+            {
+                key: "marker3",
+                position: [51.49, -0.05],
+                children: "My third popup"
+            }
+        ];
+
+        return (
+            <StyledHomeWrapper>
+                <Intro>
+                    <Wrapper>
+                        <Title color="black">
+                            Para iniciar, selecione um ponto.
+                        </Title>
+                    </Wrapper>
+                </Intro>
+                <Map center={center} zoom={this.state.zoom}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <MyMarkersList markers={markers} />
+                </Map>
+            </StyledHomeWrapper>
+        );
+    }
+}
 
 export default MapHome;
