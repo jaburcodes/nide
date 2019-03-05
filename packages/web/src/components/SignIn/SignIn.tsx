f0854597f9b902e4ababa3638f6575abec87b274
leonardomso <leonardomso11@gmail.com>
2019-03-05 14:46:17 -0300

    Starting Map

--- packages/web/src/components/SignIn/SignIn.tsx
+++ packages/web/src/components/SignIn/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import * as Yup from "yup";
 import { withFormik, FormikProps } from "formik";
 import { graphql } from "react-apollo";
--- packages/web/src/screens/Home/HomeWrapper.tsx
+++ packages/web/src/screens/Home/HomeWrapper.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import StyledHomeWrapper from "../../utils/components/Home/Home";
 import HomeBackground from "../../utils/components/Home/HomeBackground";
@@ -8,13 +9,23 @@ import Home from "./Home";
 import MapDetail from "./Map/MapDetail";
 import MapHome from "./Map/MapHome";
 
+const history = createBrowserHistory();
+
 const HomeWrapper = () => (
     <HomeBackground>
         <StyledHomeWrapper>
             <Switch>
-                <Route path="/" component={Home} />
-                <Route path="/visualizar" component={MapHome} />
-                <Route path="/visualizar/detail" component={MapDetail} />
+                <Route path="/" component={Home} history={history} />
+                <Route
+                    path="/visualizar"
+                    component={MapHome}
+                    history={history}
+                />
+                <Route
+                    path="/visualizar/detail"
+                    component={MapDetail}
+                    history={history}
+                />
             </Switch>
         </StyledHomeWrapper>
     </HomeBackground>
